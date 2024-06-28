6741721aa1d1b603d545ec3821d4cd195029ba97
markusalterauge <markus.alterauge@smail.th-koeln.de>
2024-06-28 18:41:21 +0200

    füge '/' hinzu

--- src/main/java/com/example/demo/ServiceRequestController.java
+++ src/main/java/com/example/demo/ServiceRequestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServiceRequestController {
 
-    @GetMapping("service")
+    @GetMapping("/service")
     public ServiceRequest getServiceRequest() {
         return new ServiceRequest("Louis", "M.", "louis101@example.com", "Diggah brauche nen Springboot service. Mach schnell!");
     }
