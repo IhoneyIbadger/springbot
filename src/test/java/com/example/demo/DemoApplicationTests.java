d573a403dd3ce0cbb5917fd1dee1ae579c3728e6
markusalterauge <markus.alterauge@smail.th-koeln.de>
2024-06-28 16:57:18 +0200

    override spring initializer

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.springbot.Springbot;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbotApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
